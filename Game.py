c6166bbf0672d274eb7870d9c50bc84dc9389bf4
monwe-jr <monwefrancis@gmail.com>
2023-01-18 13:53:58 -0500

    Modify Game.py script for testing purposes

--- Game.py
+++ Game.py
@@ -1,14 +1,5 @@
 from Client import Client
+from GUI import GUI
 
 game_over = False
-clientOne = Client(game_over, 1, 0)
-clientTwo = Client(game_over, 0, 1)
-turn = 0
-
-while not game_over:
-    if turn == 0:
-        clientTwo.recieve
-    else:
-        clientOne.recieve 
-    turn += 1
-    turn = turn % 2 
+grid = GUI(game_over, 1, 2)
