94de14db637fc32daf18ffb96dd92feace703d77
monwe-jr <monwefrancis@gmail.com>
2023-01-19 21:19:55 -0500

    Improve readability of CLient.py

--- Client.py
+++ Client.py
@@ -1,24 +1,35 @@
 import socket
 
 class Client:
-    global msg
+    
+    def __init__(self):
+        self.client_one = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # use IPV4
+        self.client_one.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.client_one.connect(("localhost", 3006))
+        self.client_two = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # use IPV4
+        self.client_two.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.client_two.connect(("localhost", 3006))
 
-    def __init__(self, board):
-        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # use IPV4
-        self.client.connect(("localhost", 2046))
-        while True:
-            try:
-                s=board
-                self.client.sendall((s+"\n").encode())
-                r=self.client.recv(100)
-                self.msg = r.decode().strip()
-            except: #For EOF on ctrl+d or interrupt on ctrl+c
-                break 
 
-    def recieve(self):
-        return self.msg 
+    def send(self, grid):
+        try:
+            self.client_one.sendall(grid.encode())
+            self.client_two.sendall(grid.encode())
+        except: #For EOF on ctrl+d or interrupt on ctrl+c
+            self.client.shutdown(1)
+            self.client.close()       
+
+
+    def recieve_from_one(self):
+        r =self.client_one.recv(100)
+        return r
+        
+    
+    def recieve_from_two(self):
+        r =self.client_two.recv(100)
+        return r
    
-
+    
 
 
 
